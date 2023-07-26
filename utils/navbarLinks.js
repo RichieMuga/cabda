3ed223d59be8edc58b1327efc31a065296438e3c
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-26 23:44:59 +0300

    feat: added navbar links

---
+++ utils/navbarLinks.js
@@ -0,0 +1,27 @@
+export const navbarLinks = [
+    {
+        id: 1,
+        href: '/',
+        label: 'Home',
+    },
+    {
+        id: 2,
+        href: '/about',
+        label: 'About Us',
+    },
+    {
+        id:3,
+        href: '/centralTopics',
+        label: 'Central Topics',
+    },
+    {
+        id: 4,
+        href: '/blog',
+        label: 'Contact Us',
+    },
+    {
+        id: 5,
+        href: '/ourTeam',
+        label: 'Our Team',
+    },
+]
