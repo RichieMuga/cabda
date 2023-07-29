8e80ec8f708ec6ae592eab2c2f97534cdc9d774f
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-29 11:03:27 +0300

    feat: added team links

---
+++ utils/teamLinks.js
@@ -0,0 +1,30 @@
+export const teamLinks = [
+    {
+        id: 1,
+        name: 'Lorem Ipsum',
+        title: 'Lorem Ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi.',
+        image: '',
+    },
+    {
+        id: 2,
+        name: 'Lorem Ipsum',
+        title: 'Lorem Ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi.',
+        image: '',
+    },
+    {
+        id: 3,
+        name: 'Lorem Ipsum',
+        title: 'Lorem Ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi.',
+        image: '',
+    },
+    {
+        id: 4,
+        name: 'Lorem Ipsum',
+        title: 'Lorem Ipsum',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi. Sed euismod, diam id aliquam aliquet, nunc nisl ultricies nunc, vitae aliquam nisl nunc eu nisi.',
+        image: '',
+    },
+]
