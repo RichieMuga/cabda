bd6af56428d056908ce687e5509095a1c37acbd2
Richie Mugambi <richiemugambi4@gmail.com>
2023-07-30 16:30:24 +0300

    chore: corrected mobile view of page

--- app/patnerprograms/page.js
+++ app/patnerprograms/page.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import Head from 'next/head';
 import Navbar from '../../components/Navbar';
 import Footer from '../../components/Footer';
@@ -13,15 +14,15 @@ const Patnerprograms = () => {
         <meta property="og:title" content="My page title" key="title" />
       </Head>
       <Navbar />
-      <div className='px-[10em] flex justify-center py-[3em]'>
+      <div className='flex justify-center py-[1em] md:mt-[2em] mt-[6em] mb-[2em]'>
         <Image src={patners} alt="together we are one" width={200} height={200} />
       </div>
-      <div className='px-[3em] text-center'>
+      <div className='px-[3em] text-center pb-[2em]'>
         <h1 className="py-[1em] font-bold text-3xl">Patner Programs</h1>
-        <p className="pb-[1em]">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil laborum nemo, nulla illum expedita nobis perspiciatis consequatur nisi nam officiis hic eaque ipsa quod facere maiores enim possimus reprehenderit ad.</p>
-        <button class="bg-primaryBlue hover:bg-secondaryBlue text-white font-bold py-2 px-4 rounded mb-[10em]">
+        <p className="pb-[1em]">Discover boundless possibilities!Join our partner program now to access a thriving network of innovators, exclusive resources, and expert support.Together, we&apos; ll soar to new heights, driving mutual success and amplifying our impact.Embrace collaboration, ignite growth, and be part of a dynamic community that empowers your brand.Let&apos;s forge a powerful alliance and seize opportunities for greatness.Don&apos;t wait—come aboard and let&apos;s shape a remarkable future together! </p>
+        <Link href='contactus' className="bg-primaryBlue hover:bg-secondaryBlue text-white font-bold py-2 px-4 rounded mb-[10em]">
           Contact Us
-        </button>
+        </Link>
       </div>
       <Footer />
     </>
